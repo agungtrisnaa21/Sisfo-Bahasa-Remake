a3bb9bf2a9ae3576b4afbedf314fbce1603c66ac
Agung Trisna Gunawan <agungtrisnaa@Agungs-MacBook-Air.local>
2026-05-22 15:50:32 +0700

    add sertifikat kelulusan testcase

---
+++ Scripts/Akademik/Sertifikat Kelulusan/Sertifikat Kelulusan/Script1779434085641.groovy
@@ -0,0 +1,68 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.maximizeWindow()
+
+WebUI.navigateToUrl(url)
+
+WebUI.setText(findTestObject('Portal Badiklat/Page_Sign in to Pusdiklat/input_Sistem Informasi Badiklat_username'), username)
+
+WebUI.setText(findTestObject('Portal Badiklat/Page_Sign in to Pusdiklat/input_Sistem Informasi Badiklat_password'), password)
+
+WebUI.click(findTestObject('Portal Badiklat/Page_Sign in to Pusdiklat/button_Sign In'))
+
+WebUI.click(findTestObject('Portal Badiklat/Page_Pusdiklat Badiklat/h5_Pusdiklat                        Bahasa Badiklat Kemhan'))
+
+WebUI.click(findTestObject('Portal Badiklat/Page_Pusdiklat Badiklat/h5_Pengelolaan Akademik'))
+
+WebUI.switchToWindowTitle('SISTEM INFORMASI AKADEMIK')
+
+WebUI.click(findTestObject('Akademik/Sidebar/Page_SISTEM INFORMASI AKADEMIK/span_Sertifikat Kelulusan'))
+
+WebUI.verifyElementText(findTestObject('Akademik/Dashboard/Page_SISTEM INFORMASI AKADEMIK/h2_Dashboard'), 'SERTIFIKAT KELULUSAN')
+
+WebUI.click(findTestObject('Akademik/Sertifikat Kelulusan/Page_SISTEM INFORMASI AKADEMIK/a_Tambah'))
+
+WebUI.setText(findTestObject('Akademik/Sertifikat Kelulusan/Page_SISTEM INFORMASI AKADEMIK/input_Jabatan Saat Ini_position'), 
+    'Penata Muda')
+
+WebUI.setText(findTestObject('Akademik/Sertifikat Kelulusan/Page_SISTEM INFORMASI AKADEMIK/input_Nama Peserta_fullName'), 
+    'Coyote Starrk')
+
+WebUI.setText(findTestObject('Akademik/Sertifikat Kelulusan/Page_SISTEM INFORMASI AKADEMIK/input_NIP_registrationNumber'), 
+    '983367263838')
+
+WebUI.setText(findTestObject('Akademik/Sertifikat Kelulusan/Page_SISTEM INFORMASI AKADEMIK/input_Nomor Sertfikat_certNumber'), 
+    nSertif)
+
+WebUI.setText(findTestObject('Akademik/Sertifikat Kelulusan/Page_SISTEM INFORMASI AKADEMIK/input_Pangkat_rank'), 'Pembina')
+
+WebUI.setText(findTestObject('Akademik/Sertifikat Kelulusan/Page_SISTEM INFORMASI AKADEMIK/input_Kursus - Angkatan_course'), 
+    'Kursus Mandarin')
+
+WebUI.click(findTestObject('Akademik/Sertifikat Kelulusan/Page_SISTEM INFORMASI AKADEMIK/button_Simpan'))
+
+WebUI.waitForElementVisible(findTestObject('General/Page_SISTEM INFORMASI AKADEMIK/toast'), 5)
+
+WebUI.verifyElementText(findTestObject('General/Page_SISTEM INFORMASI AKADEMIK/toast'), 'Berhasil Membuat Data Baru')
+
+WebUI.closeBrowser()
+
